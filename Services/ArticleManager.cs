a12ec4e49cdbb2d088b60c7bfa87d8bf917cb983
minh.tranvan <minh.tranvan@hdwebsoft.io>
2022-09-07 19:25:43 +0700

    add logic article page

--- Services/ArticleManager.cs
+++ Services/ArticleManager.cs
@@ -4,9 +4,12 @@ public class ArticleManager
 {
     public class NewArticle
     {
+        public int Id{ get; set;}
         public string Title { get; set; }
         public string SubTitle { get; set; }
         public string Content { get; set; }
+        
+        public DateTime DatePublish { get; set; }
         public List<String> Categories { get; set; }
         public List<String> Authors { get; set; }
         public List<NewArticle> NewArticles { get; set; }
@@ -16,6 +19,31 @@ public class ArticleManager
             Categories = new List<string>();
             Authors = new List<string>();
             NewArticles = new List<NewArticle>();
+           
+        }
+
+        public List<NewArticle> GetNewArticles(string keysearch=null)
+        {
+            List<NewArticle> list = new List<NewArticle>(){
+                new NewArticle()
+                {
+                    Id = 1,
+                    Title = "Title 1",
+                    DatePublish = DateTime.Now
+                },
+                new NewArticle()
+                {
+                    Id = 2,
+                    Title = "Title 2",
+                    DatePublish = DateTime.Now.AddDays(14)
+                }
+            };
+            if (String.IsNullOrEmpty(keysearch))
+            {
+                return list;
+            }
+
+            return list.FindAll(op => op.Title.Contains(keysearch));
         }
         public void AddNewArticle(NewArticle article)
         {
